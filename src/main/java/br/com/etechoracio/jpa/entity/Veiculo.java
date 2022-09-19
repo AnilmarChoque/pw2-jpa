95c06816a7191d39481bf9ecc0e6fdf367ab1f91
AnilmarChoque <99197806+AnilmarChoque@users.noreply.github.com>
2022-09-19 10:47:41 -0300

    Criação de Propr e Fone e inclusão de Propr em Vei
    Criação das entidades Proprietário e Telefone e Inclusa de proprietário na entidade Veiculo

--- src/main/java/br/com/etechoracio/jpa/entity/Veiculo.java
+++ src/main/java/br/com/etechoracio/jpa/entity/Veiculo.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -36,6 +39,10 @@ public class Veiculo {
 	
 	@Column(name = "VLR_PRECO")
 	private double valor;
+	
+	@JoinColumn(name = "ID_PROPRIETARIO")
+	@ManyToOne
+	private Proprietario proprietario;
     
 	
 }

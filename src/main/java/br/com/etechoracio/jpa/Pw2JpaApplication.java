064c72b5613e95db8852f69b5818185532380270
AnilmarChoque <99197806+AnilmarChoque@users.noreply.github.com>
2022-10-24 11:27:32 -0300

    Exercício incompleto

--- src/main/java/br/com/etechoracio/jpa/Pw2JpaApplication.java
+++ src/main/java/br/com/etechoracio/jpa/Pw2JpaApplication.java
@@ -1,11 +1,15 @@
 package br.com.etechoracio.jpa;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.etechoracio.jpa.entity.GrupoUsuario;
 import br.com.etechoracio.jpa.entity.Usuario;
+import br.com.etechoracio.jpa.repository.GrupoUsuarioRepository;
 import br.com.etechoracio.jpa.repository.UsuarioRepository;
 
 @SpringBootApplication
@@ -13,6 +17,8 @@ public class Pw2JpaApplication implements CommandLineRunner {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+	
+	private GrupoUsuarioRepository grupoUsuarioRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pw2JpaApplication.class, args);
@@ -26,6 +32,17 @@ public class Pw2JpaApplication implements CommandLineRunner {
 		usuarioRepository.save(usuario);
 		
 		
+		Usuario usuario1 = Usuario.builder().nome("Roberto")
+                .senha("123456")
+                .grupos(1)
+                .build();
+		grupoUsuarioRepository.save(usuario1);
+		
+		Usuario usuario2 = Usuario.builder().nome("Augusto")
+                .senha("654321")
+                .grupos(1)
+                .build();
+		grupoUsuarioRepository.save(usuario2);
 	}
 
 
